ac96022b4b1ce8ef67be972507dc9aa8ed05a9d7
primalkarma <primalkarma@gmail.com>
2025-07-19 09:19:25 +0530

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Parimal Devi on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
